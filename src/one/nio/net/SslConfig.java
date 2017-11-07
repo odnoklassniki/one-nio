123cb9fd4340103597fea1bd08f09488f36d7b82
Andrey Pangin <andrey.pangin@corp.mail.ru>
2017-11-07 20:24:22 +0300

    Compatibility layer for different versions of OpenSSL. Support for multiple server certificates.

--- src/one/nio/net/SslConfig.java
+++ src/one/nio/net/SslConfig.java
@@ -24,24 +24,26 @@ import java.util.Properties;
 @Config
 public class SslConfig {
     // Intermediate compatibility ciphersuite according to https://wiki.mozilla.org/Security/Server_Side_TLS
-    static final String DEFAULT_CIPHERS = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:ECDHE-RSA-DES-CBC3-SHA:ECDHE-ECDSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA";
+    static final String DEFAULT_CIPHERS = "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS";
+    static final int DEFAULT_CACHE_SIZE = 262144;
     static final long DEFAULT_TIMEOUT_SEC = 300;
 
     public boolean debug;
     public String protocols;
     public String ciphers;
-    public String certFile;
-    public String privateKeyFile;
+    public String[] certFile;
+    public String[] privateKeyFile;
     public String caFile;
     public String ticketKeyFile;
     public String ticketDir;
     public int verifyMode;
+    public int cacheSize;
     @Converter(method = "longTime")
     public long timeout;
     @Converter(method = "longTime")
     public long refreshInterval;
     public String sessionId;
-    public String applicationProtocols;
+    public String[] applicationProtocols;
     public String ocspFile;
 
     // The following fields should not be updated by SslContext.inherit()
@@ -52,9 +54,13 @@ public class SslConfig {
         SslConfig config = new SslConfig();
         config.protocols      = props.getProperty("one.nio.ssl.protocols");
         config.ciphers        = props.getProperty("one.nio.ssl.ciphers");
-        config.certFile       = props.getProperty("one.nio.ssl.certFile");
-        config.privateKeyFile = props.getProperty("one.nio.ssl.privateKeyFile");
+        config.certFile       = toArray(props.getProperty("one.nio.ssl.certFile"));
+        config.privateKeyFile = toArray(props.getProperty("one.nio.ssl.privateKeyFile"));
         config.ticketKeyFile  = props.getProperty("one.nio.ssl.ticketKeyFile");
         return config;
+    }
+
+    private static String[] toArray(String line) {
+        return line == null || line.isEmpty() ? null : line.split(",");
     }
 }
