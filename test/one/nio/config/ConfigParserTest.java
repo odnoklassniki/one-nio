ed006411c9780642adbe35f97d8ad495490f015f
Andrey Pangin <andrey.pangin@corp.mail.ru>
2017-11-07 20:18:02 +0300

    YAML parser now supports references and extended array/list declaration

--- test/one/nio/config/ConfigParserTest.java
+++ test/one/nio/config/ConfigParserTest.java
@@ -17,7 +17,9 @@
 package one.nio.config;
 
 import junit.framework.TestCase;
-import one.nio.server.ServerConfig;
+import one.nio.http.HttpServerConfig;
+
+import java.util.Arrays;
 
 public class ConfigParserTest extends TestCase {
     private static final String SERVER_CONFIG = "\n" +
@@ -36,16 +38,27 @@ public class ConfigParserTest extends TestCase {
             "     privateKeyFile:       /etc/ssl/my.key\n" +
             "     timeout:              12H\n" +
             " - port: 8443\n" +
-            "   ssl:\n" +
-            "     applicationProtocols: http/1.1,http/2\n" +
+            "   ssl: &id1\n" +
+            "     applicationProtocols:\n" +
+            "      - http/1.1\n" +
+            "      - http/2\n" +
+            " - port: 9443\n" +
+            "   ssl: *id1\n" +
             " - port: 80\n" +
             "   backlog: 10000\n" +
             "   deferAccept: false\n" +
             "   recvBuf: 32k\n" +
-            "   sendBuf: 1M\n";
+            "   sendBuf: 1M\n" +
+            "\n" +
+            "virtualHosts:\n" +
+            "  admin: admin.example.com\n" +
+            "  default: &id2\n" +
+            "   - example.com\n" +
+            "   - www.example.com\n" +
+            "  invalid: *id2\n";
 
     public void testConfigParser() throws Exception {
-        ServerConfig config = ConfigParser.parse(SERVER_CONFIG, ServerConfig.class);
+        HttpServerConfig config = ConfigParser.parse(SERVER_CONFIG, HttpServerConfig.class);
 
         assertEquals(120000, config.keepAlive);
         assertEquals(1000, config.maxWorkers);
@@ -55,23 +68,31 @@ public class ConfigParserTest extends TestCase {
         assertEquals(false, config.affinity);
         assertEquals(Thread.NORM_PRIORITY, config.threadPriority);
 
-        assertEquals(3, config.acceptors.length);
+        assertEquals(4, config.acceptors.length);
 
         assertEquals(443, config.acceptors[0].port);
         assertEquals(10000, config.acceptors[0].backlog);
         assertEquals(true, config.acceptors[0].deferAccept);
-        assertEquals("http/1.1", config.acceptors[0].ssl.applicationProtocols);
+        assertEquals("http/1.1", config.acceptors[0].ssl.applicationProtocols[0]);
         assertEquals("TLSv1+TLSv1.1+TLSv1.2", config.acceptors[0].ssl.protocols);
         assertEquals(43200000L, config.acceptors[0].ssl.timeout);
 
         assertEquals(8443, config.acceptors[1].port);
-        assertEquals("http/1.1,http/2", config.acceptors[1].ssl.applicationProtocols);
+        assertEquals("[http/1.1, http/2]", Arrays.toString(config.acceptors[1].ssl.applicationProtocols));
 
-        assertEquals(80, config.acceptors[2].port);
-        assertEquals(10000, config.acceptors[2].backlog);
-        assertEquals(false, config.acceptors[2].deferAccept);
-        assertNull(config.acceptors[2].ssl);
-        assertEquals(32768, config.acceptors[2].recvBuf);
-        assertEquals(1024*1024, config.acceptors[2].sendBuf);
+        assertEquals(9443, config.acceptors[2].port);
+        assertTrue(config.acceptors[1].ssl == config.acceptors[2].ssl);
+
+        assertEquals(80, config.acceptors[3].port);
+        assertEquals(10000, config.acceptors[3].backlog);
+        assertEquals(false, config.acceptors[3].deferAccept);
+        assertNull(config.acceptors[3].ssl);
+        assertEquals(32768, config.acceptors[3].recvBuf);
+        assertEquals(1024*1024, config.acceptors[3].sendBuf);
+
+        assertEquals(3, config.virtualHosts.size());
+        assertEquals("[admin.example.com]", Arrays.toString(config.virtualHosts.get("admin")));
+        assertEquals("[example.com, www.example.com]", Arrays.toString(config.virtualHosts.get("default")));
+        assertTrue(config.virtualHosts.get("default") == config.virtualHosts.get("invalid"));
     }
 }
