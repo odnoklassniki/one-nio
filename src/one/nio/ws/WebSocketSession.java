23d095ab63cda251469c719dd551483b080b334a
Vadim Yelisseyev <vadim.yelisseyev@sumsub.com>
2024-09-01 14:03:56 +0300

    added copyright header to all new files

--- src/one/nio/ws/WebSocketSession.java
+++ src/one/nio/ws/WebSocketSession.java
@@ -1,11 +1,24 @@
+/*
+ * Copyright 2015 Odnoklassniki Ltd, Mail.Ru Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package one.nio.ws;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 import one.nio.http.HttpSession;
 import one.nio.http.Request;
@@ -31,8 +44,6 @@ import one.nio.ws.message.TextMessage;
  * @author <a href="mailto:vadim.yelisseyev@gmail.com">Vadim Yelisseyev</a>
  */
 public class WebSocketSession extends HttpSession {
-    protected static final Log log = LogFactory.getLog(WebSocketSession.class);
-
     private final WebSocketServer server;
     private final WebSocketServerConfig config;
     private final List<Extension> extensions;
@@ -116,7 +127,6 @@ public class WebSocketSession extends HttpSession {
 
     @Override
     public void handleException(Throwable e) {
-        log.error(e);
         if (e instanceof WebSocketException) {
             close(((WebSocketException) e).code());
             return;
